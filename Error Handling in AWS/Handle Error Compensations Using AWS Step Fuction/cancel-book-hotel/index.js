dd6b8a6ffb980b10356a3bb3b2c3c23f5e2c5811
ozay.duman@gmail.com <ozay.duman@gmail.com>
2021-07-19 23:46:03 +0300

    Handle Error Compensations Using AWS Step Function

---
+++ Error Handling in AWS/Handle Error Compensations Using AWS Step Fuction/cancel-book-hotel/index.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (evt) => {
+   const url = process.env.BOOKING_SERVICE_URL;//'https://69yseh6gf7.execute-api.us-east-1.amazonaws.com/Prod/hotel';
+   // make request
+   // make request
+   let response = await fetch(url, {
+                     method: 'DELETE',
+                     body: JSON.stringify({"reservation_id": evt.hotel.reservation_id})
+                  });
+   
+   let json = await response.json();
+   if (response.ok) {
+      return json;
+   } else if (response.status == 418) {
+      throw new InvalidInputError(JSON.stringify(json));
+   } else if (response.status == 503) {
+      throw new TransientError(JSON.stringify(json));
+   } else {
+      throw new Error(JSON.stringify(json));
+   }
+}
+
+function InvalidInputError(message) {
+    this.name = 'InvalidInputError';
+    this.message = message;
+}
+
+function TransientError(message) {
+    this.name = 'TransientError';
+    this.message = message;
+}
+
+InvalidInputError.prototype = new Error();
+TransientError.prototype = new Error();
+
